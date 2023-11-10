ebfb2f4ec3c6c86c576c5a5a3f4064e278c901c0
eexzy <27951093+eexzy@users.noreply.github.com>
2023-11-10 01:42:03 -0600

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,70 @@
+import numpy as np
+import altair as alt
+import pandas as pd
+import streamlit as st
+
+
+st.write("Hello World")
+
+
+st.header('st.button')
+
+if st.button('Say hello'):
+     st.write('Why hello there')
+else:
+     st.write('Goodbye')
+
+st.write('Hello, ***World!*** :sunglasses:')
+st.write(1234)
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40],
+}))
+
+st.header('st.write')
+
+# Example 1
+
+st.write('Hello, *World!* :sunglasses:')
+
+# Example 2
+
+st.write(1234)
+
+# Example 3
+
+df = pd.DataFrame({
+     'first column': [1, 2, 3, 4],
+     'second column': [10, 20, 30, 40]
+     })
+st.write(df)
+
+# Example 4
+
+st.write('Below is a DataFrame:', df, 'Above is a dataframe.')
+
+# Example 5
+
+df2 = pd.DataFrame(
+     np.random.randn(200, 3),
+     columns=['a', 'b', 'c'])
+c = alt.Chart(df2).mark_circle().encode(
+     x='a', y='b', size='c', color='c', tooltip=['a', 'b', 'c'])
+st.write(c)
+
+st.write(df2)
+
+st.markdown("*Streamlit* is **really** ***cool***.")
+st.markdown('''
+    :red[Streamlit] :orange[can] :green[write] :blue[text] :violet[in]
+    :gray[pretty] :rainbow[colors].''')
+st.markdown("Here's a bouquet &mdash;\
+            :tulip::cherry_blossom::rose::hibiscus::sunflower::blossom:")
+
+multi = '''If you end a line with two spaces,
+a soft return is used for the next line.
+
+Two (or more) newline characters in a row will result in a hard return.
+'''
+st.markdown(multi)
+
